43ec5f61fc365579d1502e6246672ff0a8ec206f
motsu8e <motsu159357@gmail.com>
2026-02-08 00:01:05 +0900

    cargo fixを使ってみた

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -59,9 +59,9 @@ fn makes_copy(some_integer: i32){
  * 戻り値とスコープ
  */
 fn return_and_scope(){
-    let s1 = gives_ownership();
+    let _s1 = gives_ownership();
     let s2 = String::from("hello");
-    let s3 = takes_and_gives_back(s2);
+    let _s3 = takes_and_gives_back(s2);
 }
 
 fn gives_ownership() -> String {
