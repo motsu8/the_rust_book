d5cad51f57830ea53ff8ad840fb2a9c367b311cd
motsu8e <motsu159357@gmail.com>
2026-02-11 17:18:07 +0900

    構造体のフィールドと同じ名前の関数も定義できる

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -8,6 +8,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
 }
 
 fn main() {
@@ -17,13 +21,8 @@ fn main() {
         height: 50,
     };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.area()
-    );
-
-    println!("rect1 is {:#?}", rect1);
-
-    dbg!(&rect1);
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
 }
 
