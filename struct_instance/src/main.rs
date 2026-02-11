ade045f6db80eab33f5f9df36388a9a951132f44
motsu8e <motsu159357@gmail.com>
2026-02-11 16:28:45 +0900

    フィールドの無いユニット様構造体

--- struct_instance/src/main.rs
+++ struct_instance/src/main.rs
@@ -1,3 +1,6 @@
+/*
+ * 構造体
+ */
 struct User {
     active: bool,
     username: String,
@@ -5,8 +8,16 @@ struct User {
     sign_in_count: u64,
 }
 
+/*
+ * タプル構造体
+ */
 struct Color(i32, i32, i32);
 struct Point(i32, i32, i32);
+
+/* 
+ * ユニット様構造体
+ */
+struct AlwaysEqual;
 
 fn main() {
     let mut user1 = User {
@@ -24,6 +35,8 @@ fn main() {
 
     let black = Color(0, 0, 0);
     let origin = Point(0, 0, 0);
+
+    let subject = AlwaysEqual;
 }
 
 fn build_user(email: String, username: String) -> User {
