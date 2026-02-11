b737ef4255423a59ba0670f0148efb296e9763f5
motsu8e <motsu159357@gmail.com>
2026-02-11 19:48:37 +0900

    コイン識別例でmatch制御フローを理解する

---
+++ match_flow/src/main.rs
@@ -0,0 +1,24 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn main() {
+    let c1 = Coin::Penny;
+    let res = value_in_cents(c1);
+    println!("Coin value is: {}", res)
+}
