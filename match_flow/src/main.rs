2407acbd5f5658fc25bcb6c3e455b9412dcd08e9
motsu8e <motsu159357@gmail.com>
2026-02-11 19:55:11 +0900

    値に束縛されるパターン

--- match_flow/src/main.rs
+++ match_flow/src/main.rs
@@ -1,8 +1,14 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -13,12 +19,18 @@ fn value_in_cents(coin: Coin) -> u8 {
         }
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        } 
     }
 }
 
 fn main() {
     let c1 = Coin::Penny;
     let res = value_in_cents(c1);
-    println!("Coin value is: {}", res)
+    println!("Coin value is: {}", res);
+
+    let c2 = Coin::Quarter(UsState::Alaska);
+    println!("Coin value is: {}", value_in_cents(c2));
 }
